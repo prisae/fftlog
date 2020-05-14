9c648cf29d17752dfd057768ed788c6b45bfaba0
prisae <mail@werthmuller.org>
2020-05-14 08:35:54 +0200

    Initial attempt to pack it

---
+++ CHANGELOG.rst
@@ -0,0 +1,18 @@
+Changelog
+#########
+
+
+v0.2.0 : First packaged release
+-------------------------------
+
+**2020-05-??**
+
+- First packaged release on PyPi and conda-forge.
+
+
+v0.1.0 : Initial upload to GitHub
+---------------------------------
+
+**2016-12-05**
+
+- Initially working version uploaded to GitHub.
--- setup.py
+++ setup.py
@@ -1,14 +1,47 @@
-#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import re
+# from setuptools import setup
 from numpy.distutils.core import setup, Extension
 
-setup(name = "fftlog",
-      version = "1.0",
-      description = "Wrapper (f2py) for Fortran FFTLog (Version 13 March 2000) by Andrew J.S. Hamilton",
-      author = 'Dieter Werthmüller',
-      author_email = 'dieter.werthmuller@gmx.ch',
-      license='CC0',
-      url='https://github.com/prisae/fftlog',
-      ext_modules = [Extension(name = "fftlog", sources = ['fftlog.pyf',] +
-                     ['src/cdgamma.f', 'src/drfftb.f', 'src/drfftf.f',
-                      'src/drffti.f', 'src/fftlog.f'],)],
-      )
+# Get README and remove badges.
+readme = open('README.rst').read()
+readme = re.sub('----.*marker', '----', readme, flags=re.DOTALL)
+
+description = ("Python wrapper (f2py) for Fortran FFTLog "
+               "(Version 13 March 2000) by Andrew J.S. Hamilton")
+
+setup(
+    name='fftlog',
+    description=description,
+    long_description=readme,
+    author='Dieter Werthmüller',
+    author_email='dieter@werthmuller.org',
+    url='https://github.com/prisae/fftlog',
+    license='CC0-1.0',
+    packages=['fftlog', ],
+    ext_modules=[
+        Extension(
+            name="fftlog._fftlog",
+            sources=['fftlog/fftlog.pyf', ] +
+                    [
+                        'fftlog/src/cdgamma.f', 'fftlog/src/drfftb.f',
+                        'fftlog/src/drfftf.f', 'fftlog/src/drffti.f',
+                        'fftlog/src/fftlog.f'
+                    ],
+                 )
+        ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication',
+    ],
+    install_requires=[
+        'scipy',
+    ],
+    use_scm_version={
+        'root': '.',
+        'relative_to': __file__,
+        'write_to': os.path.join('fftlog', 'version.py'),
+    },
+    setup_requires=['setuptools_scm'],
+)

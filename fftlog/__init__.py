eed4677f0dd9b8c5f6b11ee4e47a16e44738e1ed
prisae <mail@werthmuller.org>
2020-05-16 15:32:59 +0200

    Fix scm version and flake8

--- fftlog/__init__.py
+++ fftlog/__init__.py
@@ -3,4 +3,14 @@ from fftlog._fftlog import fhti, fftl, fht, fhtq
 
 __all__ = ['fhti', 'fftl', 'fht', 'fhtq']
 
-__version__ = '0.2.0rc1'
+# Version
+try:
+    # - Released versions just tags:       1.10.0
+    # - GitHub commits add .dev#+hash:     1.10.1.dev3+g973038c
+    # - Uncommitted changes add timestamp: 1.10.1.dev3+g973038c.d20191022
+    from .version import version as __version__
+except ImportError:
+    # If it was not installed, then we don't know the version. We could throw a
+    # warning here, but this case *should* be rare. fftlog should be installed
+    # properly!
+    __version__ = 'unknown-'+datetime.today().strftime('%Y%m%d')
